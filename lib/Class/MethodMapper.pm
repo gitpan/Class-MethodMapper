b998df71813233c0c49a7b07b42e3af03367613e
Kevin A. Lenzo <lenzo@yetanother.org>
2001-03-19 12:44:06 +0000

    Import of LENZO/Class-MethodMapper-1.0 from CPAN.
    gitpan-cpan-distribution: Class-MethodMapper
    gitpan-cpan-version:      1.0
    gitpan-cpan-path:         LENZO/Class-MethodMapper-1.0.tar.gz
    gitpan-cpan-author:       LENZO
    gitpan-cpan-maturity:     released

--- lib/Class/MethodMapper.pm
+++ lib/Class/MethodMapper.pm
@@ -1,35 +1,7 @@
-###########################################################################
-##                                                                       ##
-##                   Carnegie Mellon University and                      ##
-##                   Kevin A. Lenzo and Alan W Black                     ##
-##                         Copyright (c) 2000                            ##
-##                        All Rights Reserved.                           ##
-##                                                                       ##
-##  Permission is hereby granted, free of charge, to use and distribute  ##
-##  this software and its documentation without restriction, including   ##
-##  without limitation the rights to use, copy, modify, merge, publish,  ##
-##  distribute, sublicense, and/or sell copies of this work, and to      ##
-##  permit persons to whom this work is furnished to do so, subject to   ##
-##  the following conditions:                                            ##
-##   1. The code must retain the above copyright notice, this list of    ##
-##      conditions and the following disclaimer.                         ##
-##   2. Any modifications must be clearly marked as such.                ##
-##   3. Original authors' names are not deleted.                         ##
-##   4. The authors' names are not used to endorse or promote products   ##
-##      derived from this software without specific prior written        ##
-##      permission.                                                      ##
-##                                                                       ##
-##  CARNEGIE MELLON UNIVERSITY AND THE CONTRIBUTORS TO THIS WORK         ##
-##  DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING      ##
-##  ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT   ##
-##  SHALL CARNEGIE MELLON UNIVERSITY NOR THE CONTRIBUTORS BE LIABLE      ##
-##  FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES    ##
-##  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN   ##
-##  AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,          ##
-##  ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF       ##
-##  THIS SOFTWARE.                                                       ##
-##                                                                       ##
-###########################################################################
+## Copyright (c) 2000, 2001
+## Carnegie Mellon University Sphinx Group, Kevin A. Lenzo, Alan W Black
+## This software is available under the same terms as Perl itself.
+## Thanks much to Martijn van Beers (LotR)
 
 =head1 NAME
 
@@ -84,7 +56,7 @@ serialized at save-time.
 
 
 package Class::MethodMapper;
-$Class::MethodMapper::VERSION = "0.9";
+$Class::MethodMapper::VERSION = "1.0";
 use strict;
 
 use Exporter;
